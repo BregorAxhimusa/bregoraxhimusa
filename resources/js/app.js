49b2fa2a5b1447e32d3ac94dd03bfca4966b75a0
Bregor Axhimusa <bregoraxhimusa31@gmail.com>
2024-06-02 02:02:36 +0200

    First Commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,18 +3,26 @@ import '../css/app.css';
 
 import '../../public/css/plugins.css';
 import '../../public/css/style.css';
+
+import '../../public/css/app.9458079f.css';
 import '../../public/css/font/fontello.eot';
 import '../../public/css/font/fontello.svg';
 import '../../public/css/font/fontello.ttf';
 import '../../public/css/font/fontello.woff';
 import '../../public/css/font/fontello.woff2';
 
+// import '../../public/css/js/app.3d22f681.js';
+// import '../../public/css/js/app.3d22f681.js.map';
+// import '../../public/css/js/chunk-vendors.e3733d6e.js';
+// import '../../public/css/js/chunk-vendors.e3733d6e.js.map';
+
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Portfolio';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
